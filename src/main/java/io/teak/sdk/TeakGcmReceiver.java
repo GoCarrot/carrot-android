c0a4ec140a46a358fa5977028f0333254456eb80
Pat Wilson <zerostride@gmail.com>
2016-01-28 10:36:44 -0800

    Unified functionality into processIntent()

--- src/main/java/io/teak/sdk/TeakGcmReceiver.java
+++ src/main/java/io/teak/sdk/TeakGcmReceiver.java
@@ -35,84 +35,101 @@ public final class TeakGcmReceiver extends BroadcastReceiver {
     public static final String TEAK_PUSH_RECEIVED_INTENT_ACTION_SUFFIX = ".intent.TEAK_PUSH_RECEIVED";
     public static final String TEAK_PUSH_OPENED_INTENT_ACTION_SUFFIX = ".intent.TEAK_PUSH_OPENED";
 
+    public static final int TEAK_PUSH_RECEIVED = 0;
+    public static final int TEAK_PUSH_OPENED = 1;
+    public static final int TEAK_UNKNOWN = -1;
+    public static final int TEAK_GCM_RECEIVED = 2;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
 
-        // <debug>
-        Log.d(Teak.LOG_TAG, String.format("Received intent with action %s", action));
-        // </debug>
-
         if (GCM_REGISTRATION_INTENT_ACTION.equals(action)) {
             //
         } else if (GCM_RECEIVE_INTENT_ACTION.equals(action)) {
-            processGcm(context, intent);
+            processIntent(context, intent);
         }
     }
 
-    public static boolean processGcm(final Context context, final Intent intent) {
-        if (true) { // Test for if we should handle all messages or just Teak ones
-            new AsyncTask<Void, String, Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-                    // <debug>
-                    Bundle bundle = intent.getExtras();
-                    if (bundle != null && !bundle.isEmpty()) {
-                        for (String key : bundle.keySet()) {
-                            Object value = bundle.get(key);
-                            Log.d(Teak.LOG_TAG, String.format("%s %s (%s)", key, value.toString(), value.getClass().getName()));
+    public static int processIntent(final Context context, final Intent intent) {
+        String action = intent.getAction();
+
+        if (action.endsWith(TEAK_PUSH_RECEIVED_INTENT_ACTION_SUFFIX)) {
+            // Send push received metric
+            return TEAK_PUSH_RECEIVED;
+        } else if (action.endsWith(TEAK_PUSH_OPENED_INTENT_ACTION_SUFFIX)) {
+            // Send push opened metric
+            return TEAK_PUSH_OPENED;
+        } else if (GCM_RECEIVE_INTENT_ACTION.equals(action)) {
+            if (true) { // Test for if we should handle all messages or just Teak ones
+                new AsyncTask<Void, String, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        // <debug>
+                        Bundle bundle = intent.getExtras();
+                        if (bundle != null && !bundle.isEmpty()) {
+                            for (String key : bundle.keySet()) {
+                                Object value = bundle.get(key);
+                                Log.d(Teak.LOG_TAG, String.format("%s %s (%s)", key, value.toString(), value.getClass().getName()));
+                            }
                         }
+                        // </debug>
+
+                        Intent pushReceivedIntent = new Intent(context.getPackageName() + TEAK_PUSH_RECEIVED_INTENT_ACTION_SUFFIX);
+
+                        if (true) { // TODO: Filter out data messages
+                            Intent pushOpenedIntent = new Intent(context.getPackageName() + TEAK_PUSH_OPENED_INTENT_ACTION_SUFFIX);
+
+                            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(context).setAutoCancel(true);
+
+                            PackageManager pm = context.getPackageManager();
+                            try {
+                                ApplicationInfo ai = pm.getApplicationInfo(context.getPackageName(), 0);
+                                builder.setSmallIcon(ai.icon);
+                            } catch (Exception e) {
+                                Log.e(Teak.LOG_TAG, "Unable to get icon resource id for GCM notification.");
+                            }
+
+                            int messageId = 0;
+                            if (bundle != null) {
+                                builder.setContentTitle(bundle.getString("title"));
+                                builder.setContentText(bundle.getString("message"));
+                                builder.setTicker(bundle.getString("tickerText"));
+                                try {
+                                    messageId = Integer.parseInt(bundle.getString("message_id"));
+                                } catch (Exception e) {
+                                    messageId = 0;
+                                }
+                            }
+
+                            pushReceivedIntent.putExtras(bundle);
+                            pushOpenedIntent.putExtras(bundle);
+
+                            PendingIntent pushOpenedPendingIntent = PendingIntent.getBroadcast(context, messageId, pushOpenedIntent, PendingIntent.FLAG_ONE_SHOT);
+                            builder.setContentIntent(pushOpenedPendingIntent);
+
+                            context.sendBroadcast(pushReceivedIntent);
+
+                            notificationManager.notify("TEAK", messageId, builder.build());
+
+                            // Wake the screen
+                            PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+                            PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, Teak.LOG_TAG);
+                            wakeLock.acquire();
+                            wakeLock.release();
+                        } else {
+                            pushReceivedIntent.putExtras(bundle);
+                            context.sendBroadcast(pushReceivedIntent);
+                        }
+
+                        return null;
                     }
-                    // </debug>
-
-                    Intent pushReceivedIntent = new Intent(context.getPackageName() + TEAK_PUSH_RECEIVED_INTENT_ACTION_SUFFIX);
-
-                    if (true) { // TODO: Filter out data messages
-                        Intent pushOpenedIntent = new Intent(context.getPackageName() + TEAK_PUSH_OPENED_INTENT_ACTION_SUFFIX).setClass(context, TeakGcmReceiver.class);
-
-                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context).setAutoCancel(true);
-
-                        PackageManager pm = context.getPackageManager();
-                        try {
-                            ApplicationInfo ai = pm.getApplicationInfo(context.getPackageName(), 0);
-                            builder.setSmallIcon(ai.icon);
-                        } catch (Exception e) {
-                            Log.e(Teak.LOG_TAG, "Unable to get icon resource id for GCM notification.");
-                        }
-
-                        if (bundle != null) {
-                            builder.setContentTitle(bundle.getString("title"));
-                            builder.setContentText(bundle.getString("message"));
-                            builder.setTicker(bundle.getString("tickerText"));
-                        }
-
-                        pushReceivedIntent.putExtras(bundle);
-                        pushOpenedIntent.putExtras(bundle);
-
-                        PendingIntent pushOpenedPendingIntent = PendingIntent.getBroadcast(context, bundle.getInt("message_id"), pushOpenedIntent, PendingIntent.FLAG_ONE_SHOT);
-                        builder.setContentIntent(pushOpenedPendingIntent);
-
-                        context.sendBroadcast(pushReceivedIntent);
-
-                        notificationManager.notify("TEAK", bundle != null ? bundle.getInt("message_id") : 0, builder.build());
-
-                        // Wake the screen
-                        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-                        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, Teak.LOG_TAG);
-                        wakeLock.acquire();
-                        wakeLock.release();
-                    } else {
-                        pushReceivedIntent.putExtras(bundle);
-                        context.sendBroadcast(pushReceivedIntent);
-                    }
-
-                    return null;
-                }
-            }.execute(null, null, null);
-            return true;
+                }.execute(null, null, null);
+                return TEAK_GCM_RECEIVED;
+            }
         }
 
-        return false;
+        return TEAK_UNKNOWN;
     }
 }
