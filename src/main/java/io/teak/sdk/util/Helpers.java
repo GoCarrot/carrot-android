f9771afd7f9bcf25725ddce5c88d7efe43a910ae
Pat Wilson <zerostride@gmail.com>
2016-03-04 22:39:29 -0800

    Minimum API level is 19 for full JSONObject functionality.

--- src/main/java/io/teak/sdk/util/Helpers.java
+++ src/main/java/io/teak/sdk/util/Helpers.java
@@ -15,22 +15,78 @@
 package io.teak.sdk;
 
 import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.lang.reflect.*;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 class Helpers {
-    protected static Object getBuildConfigValue(Context context, String fieldName) {
+    static Object getBuildConfigValue(Context context, String fieldName) {
         try {
             Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
             Field field = clazz.getField(fieldName);
             return field.get(null);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Log.e(Teak.LOG_TAG, Log.getStackTraceString(e));
         } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+            Log.e(Teak.LOG_TAG, Log.getStackTraceString(e));
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            Log.e(Teak.LOG_TAG, Log.getStackTraceString(e));
         }
         return null;
+    }
+
+    static Map<String, Object> jsonToMap(JSONObject json) throws JSONException {
+        Map<String, Object> retMap = new HashMap<String, Object>();
+
+        if(json != JSONObject.NULL) {
+            retMap = toMap(json);
+        }
+        return retMap;
+    }
+
+    private static Map<String, Object> toMap(JSONObject object) throws JSONException {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        Iterator keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next().toString();
+            Object value = object.get(key);
+
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    private static List<Object> toList(JSONArray array) throws JSONException {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
     }
 }
