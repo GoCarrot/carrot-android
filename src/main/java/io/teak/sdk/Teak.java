4fa14c401240845380603365cd837d27415ca79b
Pat Wilson <zerostride@gmail.com>
2016-06-30 16:22:01 -0700

    Bad rebase.

--- src/main/java/io/teak/sdk/Teak.java
+++ src/main/java/io/teak/sdk/Teak.java
@@ -118,6 +118,9 @@ public class Teak extends BroadcastReceiver {
 
         CacheManager.initialize(activity);
 
+        Teak.preferences = activity.getSharedPreferences(TEAK_PREFERENCES_FILE, Context.MODE_PRIVATE);
+        Teak.gcm = GoogleCloudMessaging.getInstance(activity.getApplicationContext());
+
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
             // TODO: This is non-ideal, see if we can find a better fallback.
             Teak.lifecycleCallbacks.onActivityCreated(activity, null);
