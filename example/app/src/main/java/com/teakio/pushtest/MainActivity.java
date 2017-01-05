8f3aa9b756492f16025ea2dfdef328ebcdca6285
Pat Wilson <zerostride@gmail.com>
2017-01-05 12:55:20 -0800

    Deep link/reward example.

--- example/app/src/main/java/com/teakio/pushtest/MainActivity.java
+++ example/app/src/main/java/com/teakio/pushtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.teakio.pushtest;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +14,47 @@ import io.teak.sdk.TeakNotification;
 
 import android.view.View;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = "Teak:Example";
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (TeakNotification.LAUNCHED_FROM_NOTIFICATION_INTENT.equals(action)) {
+                Bundle bundle = intent.getExtras();
+                try {
+
+                    HashMap<String, Object> teakReward = (HashMap<String, Object>) bundle.getSerializable("teakReward");
+                    if (teakReward != null) {
+                        Log.d(LOG_TAG, teakReward.toString());
+                    }
+
+                    if (bundle.getString("teakDeepLinkPath") != null) {
+                        Log.d(LOG_TAG, bundle.getString("teakDeepLinkPath"));
+                        HashMap<String, Object> teakDeepLinkQueryParameters = (HashMap<String, Object>) bundle.getSerializable("teakDeepLinkQueryParameters");
+                        if (teakDeepLinkQueryParameters != null) {
+                            Log.d(LOG_TAG, teakDeepLinkQueryParameters.toString());
+                        }
+                    }
+                } catch(Exception e) {
+                    Log.e(LOG_TAG, Log.getStackTraceString(e));
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Teak.onCreate(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TeakNotification.LAUNCHED_FROM_NOTIFICATION_INTENT);
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, filter);
 
         Teak.identifyUser("demo-app-thingy-3");
         //Teak.trackEvent("actionid","objecttypeid", "instanceid");
