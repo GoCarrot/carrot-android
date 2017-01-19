3085862c027a0889dc3f59a6ce94482913caa92f
Pat Wilson <zerostride@gmail.com>
2017-01-19 15:46:26 -0800

    Remove unused imports.

--- src/main/java/io/teak/sdk/ADMMessageHandler.java
+++ src/main/java/io/teak/sdk/ADMMessageHandler.java
@@ -26,10 +26,8 @@ import com.amazon.device.messaging.ADMMessageReceiver;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.MessageDigest;
 import java.util.Scanner;
 import java.util.regex.Matcher;
