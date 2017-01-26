d478c24d43734fa55e2f5f3cf879007383e455bc
Pat Wilson <zerostride@gmail.com>
2017-01-26 14:22:04 -0800

    Example now has purchase deep link

--- example/app/src/main/java/com/teakio/pushtest/MainActivity.java
+++ example/app/src/main/java/com/teakio/pushtest/MainActivity.java
@@ -1,21 +1,34 @@
 package com.teakio.pushtest;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import io.teak.sdk.DeepLink;
 import io.teak.sdk.Teak;
+import io.teak.sdk.TeakLink;
 import io.teak.sdk.TeakNotification;
 
 import android.view.View;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.android.vending.billing.IInAppBillingService;
+
+
+import org.json.JSONObject;
+
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,7 +56,54 @@ public class MainActivity extends AppCompatActivity {
                             Log.d(LOG_TAG, teakDeepLinkQueryParameters.toString());
                         }
                     }
-                } catch(Exception e) {
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, Log.getStackTraceString(e));
+                }
+            }
+        }
+    };
+
+    static AppCompatActivity mThis;
+    static IInAppBillingService mService;
+    ServiceConnection mServiceConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name,
+                                       IBinder service) {
+            mService = IInAppBillingService.Stub.asInterface(service);
+
+            // Clear inventory
+            if (true) {
+                try {
+                    Bundle ownedItems = mService.getPurchases(3, getPackageName(), "inapp", null);
+                    int response = ownedItems.getInt("RESPONSE_CODE");
+                    if (response == 0) {
+                        ArrayList<String>  purchaseDataList = ownedItems.getStringArrayList("INAPP_PURCHASE_DATA_LIST");
+
+                        for (int i = 0; i < purchaseDataList.size(); ++i) {
+                            String purchaseData = purchaseDataList.get(i);
+                            Log.d(LOG_TAG, purchaseData);
+                            try {
+                                JSONObject purchase = new JSONObject(purchaseData);
+                                mService.consumePurchase(3, getPackageName(), purchase.getString("purchaseToken"));
+                            } catch (Exception e) {
+                                Log.e(LOG_TAG, Log.getStackTraceString(e));
+                            }
+
+                            // do something with this purchase information
+                            // e.g. display the updated list of products owned by user
+                            //response =
+                        }
+
+                        // if continuationToken != null, call getPurchases again
+                        // and pass in the token to retrieve more items
+                    }
+
+                } catch (Exception e) {
                     Log.e(LOG_TAG, Log.getStackTraceString(e));
                 }
             }
@@ -56,12 +116,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) == 0) {
+            GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+            Log.d(LOG_TAG, "I guess stuff is ok? " + gcm);
+        }
+
         // Register the BroadcastReceiver defined above to listen for notification launches
         IntentFilter filter = new IntentFilter();
         filter.addAction(TeakNotification.LAUNCHED_FROM_NOTIFICATION_INTENT);
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, filter);
 
         Teak.identifyUser("demo-app-thingy-3");
+
+        try {
+            for (Method method : MainActivity.class.getDeclaredMethods()) {
+                DeepLink.validateAnnotations(method, false);
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+        }
+
+        // com.teakio.pushtest.dollar
+        mThis = this;
+        Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
+        serviceIntent.setPackage("com.android.vending");
+        bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mService != null) {
+            unbindService(mServiceConn);
+        }
+    }
+
+    @TeakLink("/store/:arg0")
+    static void fooBar(final String arg0) {
+        Log.d(LOG_TAG, "IT called the thing! " + arg0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Bundle buyIntentBundle = mService.getBuyIntent(3, mThis.getPackageName(), arg0, "inapp", "");
+                    PendingIntent pendingIntent = buyIntentBundle.getParcelable("BUY_INTENT");
+                    mThis.startIntentSenderForResult(pendingIntent.getIntentSender(), 1001, new Intent(), 0, 0, 0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -83,6 +188,22 @@ public class MainActivity extends AppCompatActivity {
             Method m = cls.getMethod("checkActivityResultForPurchase", int.class, Intent.class);
             m.invoke(null, resultCode, data);
         } catch (Exception ignored) {} */
+
+        if (requestCode == 1001) {
+            int responseCode = data.getIntExtra("RESPONSE_CODE", 0);
+            String purchaseData = data.getStringExtra("INAPP_PURCHASE_DATA");
+            String dataSignature = data.getStringExtra("INAPP_DATA_SIGNATURE");
+
+            if (resultCode == RESULT_OK) {
+                try {
+                    JSONObject jo = new JSONObject(purchaseData);
+                    String sku = jo.getString("productId");
+                    int response = mService.consumePurchase(3, getPackageName(), jo.getString("purchaseToken"));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
