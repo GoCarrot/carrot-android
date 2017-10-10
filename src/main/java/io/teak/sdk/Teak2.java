d8afff63869730e63092012dc63a70e11526559e
Pat Wilson <zerostride@gmail.com>
2017-10-10 15:32:30 -0700

    Add copyright/license

--- src/main/java/io/teak/sdk/Teak2.java
+++ src/main/java/io/teak/sdk/Teak2.java
@@ -1,4 +1,4 @@
-/* Teak -- Copyright (C) 2016 GoCarrot Inc.
+/* Teak -- Copyright (C) 2017 GoCarrot Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.teak.sdk;
 
 import android.app.Activity;
