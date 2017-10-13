3925a243f00e56e0a2b15bdd5902bb419f4d9fe8
Pat Wilson <zerostride@gmail.com>
2017-10-13 15:37:11 -0700

    Return canceled notifications

--- src/main/java/io/teak/sdk/TeakNotification.java
+++ src/main/java/io/teak/sdk/TeakNotification.java
@@ -551,6 +551,7 @@ public class TeakNotification {
                             contents.put("status", response.getString("status"));
 
                             if (response.getString("status").equals("ok")) {
+                                contents.put("data", response.getJSONArray("canceled"));
                                 Teak.log.i("notification.cancel_all", "Canceled all notifications.");
                             } else {
                                 Teak.log.e("notification.cancel_all.error", "Error canceling all notifications.", _.h("response", response.toString()));
