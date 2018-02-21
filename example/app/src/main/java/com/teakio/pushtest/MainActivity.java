0c57819759020a99fe3bea4ff5d5d9afdb3773b2
Pat Wilson <zerostride@gmail.com>
2018-02-21 17:18:00 -0600

    Removing imports

--- example/app/src/main/java/com/teakio/pushtest/MainActivity.java
+++ example/app/src/main/java/com/teakio/pushtest/MainActivity.java
@@ -16,7 +16,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,9 +29,7 @@ import io.teak.sdk.event.PushNotificationEvent;
 
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.vending.billing.IInAppBillingService;
 
