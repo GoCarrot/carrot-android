8108e49fb27ca61919aa65183490b149cf5ea670
Pat Wilson <zerostride@gmail.com>
2018-07-06 16:55:55 -0700

    Adobe AIR opt out implementation

--- src/main/java/io/teak/sdk/wrapper/air/IdentifyUserFunction.java
+++ src/main/java/io/teak/sdk/wrapper/air/IdentifyUserFunction.java
@@ -18,13 +18,24 @@ import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
 
+import java.util.Arrays;
+
 import io.teak.sdk.Teak;
+import io.teak.sdk.json.JSONArray;
 
 public class IdentifyUserFunction implements FREFunction {
     @Override
     public FREObject call(FREContext context, FREObject[] argv) {
+        String[] optOut = new String[]{};
         try {
-            Teak.identifyUser(argv[0].getAsString());
+            final JSONArray jsonList = new JSONArray(argv[1].getAsString());
+            optOut = Arrays.copyOf(jsonList.toList().toArray(), jsonList.length(), String[].class);
+        } catch (Exception e) {
+            Teak.log.exception(e);
+        }
+
+        try {
+            Teak.identifyUser(argv[0].getAsString(), optOut);
         } catch (Exception e) {
             Teak.log.exception(e);
         }
