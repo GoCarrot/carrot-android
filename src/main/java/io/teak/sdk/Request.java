3f0541a18b5383ad0e72f8f46234b9baaeb523ca
Pat Wilson <zerostride@gmail.com>
2019-03-25 11:28:36 -0700

    Unused import

--- src/main/java/io/teak/sdk/Request.java
+++ src/main/java/io/teak/sdk/Request.java
@@ -2,7 +2,6 @@ package io.teak.sdk;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.Base64;
 
 import javax.crypto.Mac;
