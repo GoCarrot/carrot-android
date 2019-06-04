7ca568f05b943161cd7d42b37687ada2214076fd
Pat Wilson <pat@teak.io>
2019-06-04 12:07:32 -0700

    Reformatting

--- src/main/java/io/teak/sdk/IntegrationChecker.java
+++ src/main/java/io/teak/sdk/IntegrationChecker.java
@@ -4,26 +4,34 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
-
+import io.teak.sdk.configuration.AppConfiguration;
+import io.teak.sdk.configuration.RemoteConfiguration;
+import io.teak.sdk.core.ThreadFactory;
+import io.teak.sdk.event.RemoteConfigurationEvent;
+import io.teak.sdk.io.ManifestParser;
+import io.teak.sdk.json.JSONArray;
+import io.teak.sdk.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import io.teak.sdk.configuration.AppConfiguration;
-import io.teak.sdk.configuration.RemoteConfiguration;
-import io.teak.sdk.event.RemoteConfigurationEvent;
-import io.teak.sdk.io.ManifestParser;
+import javax.net.ssl.HttpsURLConnection;
 
 public class IntegrationChecker {
     public static final String LOG_TAG = "Teak.Integration";
