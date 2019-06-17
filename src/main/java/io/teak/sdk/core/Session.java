02cff17075ca53ebbcdbf6d9ea48239032ee29d2
Pat Wilson <pat@teak.io>
2019-06-17 14:02:04 -0700

    Change to X-Teak-Supports-Templates

--- src/main/java/io/teak/sdk/core/Session.java
+++ src/main/java/io/teak/sdk/core/Session.java
@@ -988,7 +988,7 @@ public class Session {
                             connection.setUseCaches(false);
                             connection.setRequestProperty("Accept-Charset", "UTF-8");
                             connection.setRequestProperty("X-Teak-DeviceType", "API");
-                            connection.setRequestProperty("X-Teak-SupportsTemplates", "TRUE");
+                            connection.setRequestProperty("X-Teak-Supports-Templates", "TRUE");
 
                             // Get Response
                             InputStream is;
