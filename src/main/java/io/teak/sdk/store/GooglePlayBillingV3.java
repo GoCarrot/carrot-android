2ea217c2e5caa1925bd8603c89965e8ea05073df
Pat Wilson <pat@teak.io>
2021-05-11 13:36:17 -0700

    Set ourselves up as purchases updated listener to start
    App should overwrite this after initialization, but we still need a pointer to the client to query the sku prices

--- src/main/java/io/teak/sdk/store/GooglePlayBillingV3.java
+++ src/main/java/io/teak/sdk/store/GooglePlayBillingV3.java
@@ -4,9 +4,10 @@ import android.content.Context;
 
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
 import com.android.billingclient.api.SkuDetails;
 import com.android.billingclient.api.SkuDetailsParams;
-import com.android.billingclient.api.SkuDetailsResponseListener;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -20,11 +21,14 @@ import io.teak.sdk.TeakEvent;
 import io.teak.sdk.event.PurchaseEvent;
 import io.teak.sdk.json.JSONObject;
 
-public class GooglePlayBillingV3 implements IStore {
+public class GooglePlayBillingV3 implements IStore, PurchasesUpdatedListener {
     private final BillingClient billingClient;
 
     public GooglePlayBillingV3(Context context) {
-        this.billingClient = BillingClient.newBuilder(context).build();
+        this.billingClient = BillingClient.newBuilder(context)
+                .setListener(this)
+                .enablePendingPurchases()
+                .build();
         Teak.log.i("billing.google.v3", "Google Play Billing v3 registered.");
     }
 
@@ -32,7 +36,7 @@ public class GooglePlayBillingV3 implements IStore {
     public void processPurchase(String purchaseDataAsString, Map<String, Object> extras) {
         final JSONObject purchaseData = new JSONObject(purchaseDataAsString);
         final String sku = (String) purchaseData.get("productId");
-        final Map<String, Object> payload = (extras == null ? new HashMap<String, Object>() : extras);
+        final Map<String, Object> payload = (extras == null ? new HashMap<>() : extras);
         payload.put("purchase_token", purchaseData.get("purchaseToken"));
         payload.put("purchase_time", purchaseData.get("purchaseTime"));
         payload.put("product_id", sku);
@@ -44,17 +48,19 @@ public class GooglePlayBillingV3 implements IStore {
                 .newBuilder()
                 .setSkusList(Collections.singletonList(sku))
                 .build();
-        this.billingClient.querySkuDetailsAsync(params, new SkuDetailsResponseListener() {
-            @Override
-            public void onSkuDetailsResponse(@NonNull BillingResult billingResult, @Nullable List<SkuDetails> list) {
-                if (list != null && !list.isEmpty()) {
-                    final SkuDetails skuDetails = list.get(0);
-                    payload.put("price_amount_micros", skuDetails.getPriceAmountMicros());
-                    payload.put("price_currency_code", skuDetails.getPriceCurrencyCode());
-                }
-
-                TeakEvent.postEvent(new PurchaseEvent(payload));
+        this.billingClient.querySkuDetailsAsync(params, (billingResult, list) -> {
+            if (list != null && !list.isEmpty()) {
+                final SkuDetails skuDetails = list.get(0);
+                payload.put("price_amount_micros", skuDetails.getPriceAmountMicros());
+                payload.put("price_currency_code", skuDetails.getPriceCurrencyCode());
             }
+
+            TeakEvent.postEvent(new PurchaseEvent(payload));
         });
+    }
+
+    @Override
+    public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> list) {
+        Teak.log.i("billing.google.v3", "onPurchasesUpdated" + billingResult.toString());
     }
 }
