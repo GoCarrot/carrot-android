f60f0567f73a93e33171cbe0a4005bd1935955d0
Ezri Coniglio <ezri@teak.io>
2023-05-03 09:04:51 -0700

    Add categories support to ChannelStatus

--- src/main/java/io/teak/sdk/core/ChannelStatus.java
+++ src/main/java/io/teak/sdk/core/ChannelStatus.java
@@ -1,5 +1,7 @@
 package io.teak.sdk.core;
 
+import java.util.Map;
+
 import io.teak.sdk.Teak;
 import io.teak.sdk.json.JSONObject;
 
@@ -9,21 +11,27 @@ import io.teak.sdk.json.JSONObject;
 public class ChannelStatus {
     public final Teak.Channel.State state;
     public final boolean deliveryFault;
+    public final Map<String, Object> categories;
 
-    public static final ChannelStatus Unknown = new ChannelStatus("unknown", false);
+    public static final ChannelStatus Unknown = new ChannelStatus("unknown", null,false);
 
-    protected ChannelStatus(final String state, final boolean deliveryFault) {
+    protected ChannelStatus(final String state, final Map<String, Object> categories, final boolean deliveryFault) {
         this.state = Teak.Channel.State.fromString(state);
+        this.categories = categories;
         this.deliveryFault = deliveryFault;
     }
 
     public static ChannelStatus fromJSON(final JSONObject jsonObject) {
-        return new ChannelStatus(jsonObject.optString("state", "unknown"), jsonObject.optBoolean("delivery_fault", false));
+        final JSONObject categories = jsonObject.optJSONObject("categories");
+        return new ChannelStatus(jsonObject.optString("state", "unknown"),
+                categories == null ? null : categories.toMap(),
+                jsonObject.optBoolean("delivery_fault", false));
     }
 
     public JSONObject toJSON() {
         final JSONObject json = new JSONObject();
         json.put("state", this.state.name);
+        json.put("categories", this.categories);
         json.put("delivery_fault", this.deliveryFault);
         return json;
     }
