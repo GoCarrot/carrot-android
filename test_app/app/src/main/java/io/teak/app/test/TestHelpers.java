58f19600855bd5a39bbcaa33fce7aea8de0fc817
Alex Scarborough <alex@teak.io>
2023-07-19 08:30:17 -0700

    Do less horrific magic in TestHelpers.

--- test_app/app/src/main/java/io/teak/app/test/TestHelpers.java
+++ test_app/app/src/main/java/io/teak/app/test/TestHelpers.java
@@ -11,31 +11,21 @@ class TestHelpers {
     static void resetTeakEventListeners() throws NoSuchFieldException, IllegalAccessException {
         Field f = TeakEvent.class.getDeclaredField("eventListeners");
         f.setAccessible(true);
+        TeakEvent.EventListeners listeners = ((TeakEvent.EventListeners) f.get(null));
 
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-
-        f.set(null, new TeakEvent.EventListeners());
+        f = TeakEvent.EventListeners.class.getDeclaredField("eventListeners");
+        f.setAccessible(true);
+        f.set(listeners, new ArrayList<TeakEvent.EventListener>());
     }
 
     static void resetTeakConfiguration() throws NoSuchFieldException, IllegalAccessException {
         Field f = TeakConfiguration.class.getDeclaredField("eventListeners");
         f.setAccessible(true);
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-
-        f.set(null, new ArrayList<TeakConfiguration.EventListener>());
+        ArrayList<TeakConfiguration.EventListener> listeners = ((ArrayList<TeakConfiguration.EventListener>) f.get(null));
+        listeners.clear();
 
         f = TeakConfiguration.class.getDeclaredField("Instance");
         f.setAccessible(true);
-
-        modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(f, f.getModifiers() & ~Modifier.FINAL);
-
         f.set(null, null);
     }
 }
